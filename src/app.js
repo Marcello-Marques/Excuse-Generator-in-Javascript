b9e7a1cb2d22b3594feaec27a423a9d53b360a6d
Marcello-Marques <137443029+Marcello-Marques@users.noreply.github.com>
2023-08-01 01:04:54 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const randomIndexWho = Math.floor(Math.random() * who.length);
+  const randomIndexAction = Math.floor(Math.random() * action.length);
+  const randomIndexWhat = Math.floor(Math.random() * what.length);
+  const randomIndexWhen = Math.floor(Math.random() * when.length);
+  let phrase = `${who[randomIndexWho]} ${action[randomIndexAction]} ${what[randomIndexWhat]} ${when[randomIndexWhen]}`;
+
+  const targeHeading = document.querySelector("#excuse");
+  targeHeading.textContent = phrase;
 };
